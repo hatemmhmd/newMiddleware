a38208b98dc2c7a6497b9dfbc75162f4f2dd8002
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-10 15:42:21 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,12 +1,139 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
+import data from '../../Data/SchemaDB.json';
+import './FilterPanel.css';
+import { useTable } from '../CustomHook/CustomHook';
+
+interface Column {
+  columnId: number;
+  columnName: string;
+  dataType: string;
+}
+
+interface Table {
+  tableId: number;
+  tableName: string;
+  primaryKey: string;
+  description: string;
+  columns: Column[];
+}
+
+interface Database {
+  id: number;
+  name: string;
+  tables: Table[];
+}
+
+const FilterPanel = () => {
+  const [selectedDatabase, setSelectedDatabase] = useState<Database>();
+  const [disabled, setDisabled] = useState(true);
+  const [showDescription, setShowDescription] = useState(false);
+  const [disabledTable, setDisabledTable] = useState(true);
+  const { selectedTable, setSelectedTable, setFilterColumn, setShowData } = useTable();
+
+  const handleDatabaseChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const databaseId = parseInt(event.target.value);
+    const database = data.find(db => db.id === databaseId);
+    setSelectedDatabase(database);
+    if (event.target.value == "NULL") {
+      setShowDescription(false);
+      setShowData(false);
+      setDisabledTable(true);
+    } else {
+      setDisabledTable(false);
+    }
+  };
+
+  const handleTableChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const tableId = parseInt(event.target.value);
+    const table = selectedDatabase?.tables.find(tb => tb.tableId === tableId);
+    setSelectedTable(table);
+    if (event.target.value == "NULL") {
+      setDisabled(true);
+      setShowData(false);
+    } else {
+      setDisabled(false);
+    }
+  };
+
+  const FilterData = () => {
+    setFilterColumn(selectedTable ? selectedTable.tableName : "");
+    setShowData(true);
+  }
+
+  return (
+    <div className='NavBar'>
+      <div className='logo'>
+        <p>middleware<br />system</p>
+      </div>
+
+      <div className='chooses'>
+        <div className='dbSelect'>
+          <select onChange={handleDatabaseChange}>
+            <option value="NULL">Select a database</option>
+            {data.map(db => (
+              <option key={db.id} value={db.id}>{db.name}</option>
+            ))}
+          </select>
+          <i className="bi bi-caret-down"></i>
+        </div>
+
+        <div className='tableSelect'>
+          <select onChange={handleTableChange} disabled={disabledTable}>
+            <option value="NULL">Select a table</option>
+            {selectedDatabase?.tables.map(tb => (
+              <option key={tb.tableId} value={tb.tableId}>{tb.tableName}</option>
+            ))}
+          </select>
+          <i className="bi bi-caret-down"></i>
+        </div>
+
+        <div className='description'>
+          <button onClick={() => { setShowDescription(prev => !prev) }} className={disabled || !selectedDatabase ? "disabled" : ""}>description</button>
+          {!disabled && showDescription && selectedDatabase && (
+            <div className='content'>
+              <i className="bi bi-caret-up-fill"></i>
+              <h3>{selectedTable?.description}</h3>
+            </div>
+          )}
+        </div>
+
+        <div className='preview'>
+          <button onClick={FilterData} className={disabled || !selectedDatabase ? "disabledPreview" : ""}>preview</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterPanel;
+
+
+
+---------
+
+
+
+import React, { useCallback, useState, useEffect } from 'react';
 import Button from 'devextreme-react/button';
 import DataGrid, {
   Column, Editing, Paging, Lookup,
 } from 'devextreme-react/data-grid';
+import { useTable } from '../CustomHook/CustomHook';
+import userData from '../../Data/UserData.json';
+import NotFound from '../../Images/no-data-icon.svg';
+import './DataTable.css';
 
-import { employees, states } from './data.ts';
+const DataTable = () => {
+  const { selectedTable, showData, filterColumn } = useTable();
+  const [data, setData] = useState([]);
 
-const App = () => {
+  useEffect(() => {
+    if (filterColumn) {
+      const tableData = userData.find((e) => e.tableName === filterColumn)?.data || [];
+      setData(tableData);
+    }
+  }, [filterColumn]);
+
   const [events, setEvents] = useState([]);
 
   const logEvent = useCallback((eventName: string) => {
@@ -18,66 +145,60 @@ const App = () => {
   }, []);
 
   return (
-    <React.Fragment>
-      <DataGrid
-        id="gridContainer"
-        dataSource={employees}
-        keyExpr="ID"
-        allowColumnReordering={true}
-        showBorders={true}
-        onEditingStart={() => logEvent('EditingStart')}
-        onInitNewRow={() => logEvent('InitNewRow')}
-        onRowInserting={() => logEvent('RowInserting')}
-        onRowInserted={() => logEvent('RowInserted')}
-        onRowUpdating={() => logEvent('RowUpdating')}
-        onRowUpdated={() => logEvent('RowUpdated')}
-        onRowRemoving={() => logEvent('RowRemoving')}
-        onRowRemoved={() => logEvent('RowRemoved')}
-        onSaving={() => logEvent('Saving')}
-        onSaved={() => logEvent('Saved')}
-        onEditCanceling={() => logEvent('EditCanceling')}
-        onEditCanceled={() => logEvent('EditCanceled')}>
-
-        <Paging enabled={true} />
-        <Editing
-          mode="row"
-          allowUpdating={true}
-          allowDeleting={true}
-          allowAdding={true} />
-
-        <Column dataField="Prefix" caption="Title" />
-        <Column dataField="FirstName" />
-        <Column dataField="LastName" />
-        <Column dataField="Position" width={130} />
-        <Column
-          dataField="StateID"
-          caption="State"
-          width={125}
-        >
-          <Lookup dataSource={states} displayExpr="Name" valueExpr="ID" />
-        </Column>
-        <Column
-          dataField="BirthDate"
-          width={125}
-          dataType="date" />
-      </DataGrid>
-
-      <div id="events">
-        <div>
-          <div className="caption">Fired events</div>
-          <Button id="clear" text="Clear" onClick={clearEvents} />
+    <>
+      {!showData && (
+        <div className="notFound">
+          <img src={NotFound} alt="NotFound" />
+          <p>Please select a database and table</p>
         </div>
-        <ul>
-          {events.map((event, index) => <li key={index}>{event}</li>)}
-        </ul>
-      </div>
-    </React.Fragment>
+      )}
+      {showData && (
+        <div className="viewData">
+          <DataGrid
+            id="gridContainer"
+            dataSource={data}
+            keyExpr="ID"
+            allowColumnReordering={true}
+            showBorders={true}
+            onEditingStart={() => logEvent('EditingStart')}
+            onInitNewRow={() => logEvent('InitNewRow')}
+            onRowInserting={() => logEvent('RowInserting')}
+            onRowInserted={() => logEvent('RowInserted')}
+            onRowUpdating={() => logEvent('RowUpdating')}
+            onRowUpdated={() => logEvent('RowUpdated')}
+            onRowRemoving={() => logEvent('RowRemoving')}
+            onRowRemoved={() => logEvent('RowRemoved')}
+            onSaving={() => logEvent('Saving')}
+            onSaved={() => logEvent('Saved')}
+            onEditCanceling={() => logEvent('EditCanceling')}
+            onEditCanceled={() => logEvent('EditCanceled')}
+          >
+            <Paging enabled={true} />
+            <Editing
+              mode="row"
+              allowUpdating={true}
+              allowDeleting={true}
+              allowAdding={true}
+            />
+            {selectedTable?.columns.map((column) => (
+              <Column key={column.columnId} dataField={column.columnName} />
+            ))}
+          </DataGrid>
+
+          <div id="events">
+            <div>
+              <div className="caption">Fired events</div>
+              <Button id="clear" text="Clear" onClick={clearEvents} />
+            </div>
+            <ul>
+              {events.map((event, index) => <li key={index}>{event}</li>)}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
-export default App;
-
-
--------------------------------------
-
-
+export default DataTable;
+  
