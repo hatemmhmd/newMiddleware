f037b64f04dd9741fe6818131549a2b739c64ef4
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-10 15:37:42 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,147 +1,83 @@
-import React, { useMemo, useState } from 'react';
-import { useTable, useRowSelect, usePagination } from 'react-table';
-import EditableCell from './EditableCell';
-import NotFound from '../../Images/no-data-icon.svg';
-import userData from '../../Data/UserData.json';
-import { useTable as useTableContext } from '../CustomHook/CustomHook';
-import './DataTable.css';
+import React, { useCallback, useState } from 'react';
+import Button from 'devextreme-react/button';
+import DataGrid, {
+  Column, Editing, Paging, Lookup,
+} from 'devextreme-react/data-grid';
 
-const DataTable = () => {
-  const { selectedTable, showData, filterColumn } = useTableContext();
-  const dataFilter = userData.find((e) => e.tableName === filterColumn)?.data || [];
+import { employees, states } from './data.ts';
 
-  const [data, setData] = useState(dataFilter);
+const App = () => {
+  const [events, setEvents] = useState([]);
 
-  const columns = useMemo(() => {
-    if (!selectedTable) return [];
-    return [
-      ...selectedTable.columns.map((column) => ({
-        Header: column.columnName,
-        accessor: column.columnName,
-        Cell: EditableCell,
-      })),
-      {
-        Header: 'Actions',
-        accessor: 'actions',
-        Cell: ({ row }) => (
-          <div>
-            <button onClick={() => handleDelete(row.index)}>Delete</button>
-          </div>
-        ),
-      },
-    ];
-  }, [selectedTable]);
+  const logEvent = useCallback((eventName: string) => {
+    setEvents((previousEvents) => [eventName, ...previousEvents]);
+  }, []);
 
-  const handleDelete = (index) => {
-    setData((oldData) => oldData.filter((_, i) => i !== index));
-  };
-
-  const updateMyData = (rowIndex, columnId, value) => {
-    setData((oldData) =>
-      oldData.map((row, index) => {
-        if (index === rowIndex) {
-          return {
-            ...row,
-            [columnId]: value,
-          };
-        }
-        return row;
-      })
-    );
-  };
-
-  const tableInstance = useTable(
-    {
-      columns,
-      data,
-      updateMyData,
-    },
-    usePagination,
-    useRowSelect
-  );
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    page,
-    state: { pageIndex, pageSize },
-    gotoPage,
-    nextPage,
-    previousPage,
-    setPageSize,
-    canNextPage,
-    canPreviousPage,
-  } = tableInstance;
+  const clearEvents = useCallback(() => {
+    setEvents([]);
+  }, []);
 
   return (
-    <>
-      {!showData && (
-        <div className="notFound">
-          <img src={NotFound} alt="NotFound" />
-          <p>Please select a database and table</p>
+    <React.Fragment>
+      <DataGrid
+        id="gridContainer"
+        dataSource={employees}
+        keyExpr="ID"
+        allowColumnReordering={true}
+        showBorders={true}
+        onEditingStart={() => logEvent('EditingStart')}
+        onInitNewRow={() => logEvent('InitNewRow')}
+        onRowInserting={() => logEvent('RowInserting')}
+        onRowInserted={() => logEvent('RowInserted')}
+        onRowUpdating={() => logEvent('RowUpdating')}
+        onRowUpdated={() => logEvent('RowUpdated')}
+        onRowRemoving={() => logEvent('RowRemoving')}
+        onRowRemoved={() => logEvent('RowRemoved')}
+        onSaving={() => logEvent('Saving')}
+        onSaved={() => logEvent('Saved')}
+        onEditCanceling={() => logEvent('EditCanceling')}
+        onEditCanceled={() => logEvent('EditCanceled')}>
+
+        <Paging enabled={true} />
+        <Editing
+          mode="row"
+          allowUpdating={true}
+          allowDeleting={true}
+          allowAdding={true} />
+
+        <Column dataField="Prefix" caption="Title" />
+        <Column dataField="FirstName" />
+        <Column dataField="LastName" />
+        <Column dataField="Position" width={130} />
+        <Column
+          dataField="StateID"
+          caption="State"
+          width={125}
+        >
+          <Lookup dataSource={states} displayExpr="Name" valueExpr="ID" />
+        </Column>
+        <Column
+          dataField="BirthDate"
+          width={125}
+          dataType="date" />
+      </DataGrid>
+
+      <div id="events">
+        <div>
+          <div className="caption">Fired events</div>
+          <Button id="clear" text="Clear" onClick={clearEvents} />
         </div>
-      )}
-      {showData && (
-        <div className="viewData">
-          <table {...getTableProps()}>
-            <thead>
-              {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                  ))}
-                </tr>
-              ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-              {page.map((row) => {
-                prepareRow(row);
-                return (
-                  <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => (
-                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                    ))}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          <div className="pagination">
-            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-              {'<<'}
-            </button>
-            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-              {'<'}
-            </button>
-            <button onClick={() => nextPage()} disabled={!canNextPage}>
-              {'>'}
-            </button>
-            <button onClick={() => gotoPage(Math.ceil(data.length / pageSize) - 1)} disabled={!canNextPage}>
-              {'>>'}
-            </button>
-            <span>
-              Page{' '}
-              <strong>
-                {pageIndex + 1} of {Math.ceil(data.length / pageSize)}
-              </strong>{' '}
-            </span>
-            <select
-              value={pageSize}
-              onChange={(e) => setPageSize(Number(e.target.value))}
-            >
-              {[10, 20, 30, 40, 50].map((size) => (
-                <option key={size} value={size}>
-                  Show {size}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-      )}
-    </>
+        <ul>
+          {events.map((event, index) => <li key={index}>{event}</li>)}
+        </ul>
+      </div>
+    </React.Fragment>
   );
 };
 
-export default DataTable;
+export default App;
+
+
+-------------------------------------
+
+
