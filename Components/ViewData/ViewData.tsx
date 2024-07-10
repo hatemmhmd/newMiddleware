717431f2496fc2fbb9dbc154faaefa7b3c1cfdb4
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-10 10:20:34 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,191 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  IconButton,
-  TextField,
-  Button,
-  MenuItem,
-  Select,
-  FormControl,
-  InputLabel,
-} from '@mui/material';
-import { Edit, Delete, Save } from '@mui/icons-material';
-import './DataTable.css'; // Ensure this file exists or remove this line if not needed
-import userData from '../../Data/UserData.json'; // Adjust path as needed
-import NotFound from '../../Images/no-data-icon.svg'; // Ensure this file exists or replace with a valid path
+import React, { useState } from 'react';
+import data from '../../Data/SchemaDB.json';
+import './FilterPanel.css';
+import { useTable } from '../CustomHook/CustomHook';
 
-interface TableRowData {
-  [key: string]: any;
+interface Column {
+    columnId: number;
+    columnName: string;
+    dataType: string;
+}
+interface Table {
+    tableId: number;
+    tableName: string;
+    primaryKey: string;
+    description: string;
+    columns: Column[];
+}
+interface Database {
+    id: number;
+    name: string;
+    tables: Table[];
 }
 
-const useTable = () => {
-  const [selectedTable, setSelectedTable] = useState<any>(null);
-  const [showData, setShowData] = useState<boolean>(false);
-  const [filterColumn, setFilterColumn] = useState<string>('');
-  const [selectedDatabase, setSelectedDatabase] = useState<string>('');
 
-  const handleSelectTable = (table: any) => {
-    setSelectedTable(table);
-    setShowData(true);
-  };
+const FilterPanel = () => {
 
-  const handleFilterColumn = (column: string) => {
-    setFilterColumn(column);
-  };
+    const [selectedDatabase, setSelectedDatabase] = useState<Database>();
+    const [disabled, setDisabled] = useState(true);
+    const [showDescription, setShowDescription] = useState(false);
+    const [disabledTable , setDisabledTable] = useState(true);
+    const {selectedTable, setSelectedTable, setFilterColumn , setShowData} = useTable();
 
-  const handleSelectDatabase = (database: string) => {
-    setSelectedDatabase(database);
-    setShowData(false); // Reset the table view when changing database
-  };
+    const handleDatabaseChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const databaseId = parseInt(event.target.value);
+        const database = data.find(db => db.id === databaseId);
+        setSelectedDatabase(database);
+        if(event.target.value == "NULL"){
+            setShowDescription(false);
+            setShowData(false);
+            setDisabledTable(true)
+        }
+        else{
+            setDisabledTable(false);
+        }
+    };
 
-  return { selectedTable, showData, filterColumn, handleSelectTable, handleFilterColumn, selectedDatabase, handleSelectDatabase };
-};
+    const handleTableChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const tableId = parseInt(event.target.value);
+        const table = selectedDatabase?.tables.find(tb => tb.tableId === tableId);
+        setSelectedTable(table);
+        if(event.target.value == "NULL"){
+            setDisabled(true);
+            setShowData(false);
+        }
+        else{
+            setDisabled(false);
+        }
+    };
 
-const DataTable: React.FC = () => {
-  const { selectedTable, showData, filterColumn, handleSelectTable, handleFilterColumn, selectedDatabase, handleSelectDatabase } = useTable();
-
-  const dataFilter = userData.find((e) => e.tableName === filterColumn)?.data || [];
-
-  const [tableData, setTableData] = useState<TableRowData[]>([]);
-  const [editRowId, setEditRowId] = useState<number | null>(null);
-  const [editFormData, setEditFormData] = useState<TableRowData>({});
-
-  useEffect(() => {
-    setTableData(dataFilter);
-  }, [dataFilter]);
-
-  const handleEditClick = (row: TableRowData, rowIndex: number) => {
-    setEditRowId(rowIndex);
-    setEditFormData({ ...row });
-  };
-
-  const handleDeleteClick = (rowIndex: number) => {
-    const updatedData = tableData.filter((_, index) => index !== rowIndex);
-    setTableData(updatedData);
-  };
-
-  const handleSaveClick = () => {
-    const updatedData = tableData.map((row, index) =>
-      index === editRowId ? { ...editFormData } : row
-    );
-    setTableData(updatedData);
-    setEditRowId(null);
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, field: string) => {
-    const value = e.target.value;
-    setEditFormData(prevState => ({
-      ...prevState,
-      [field]: value
-    }));
-  };
-
-  const handlePreviewClick = () => {
-    const selectedTable = userData.find((table) => table.tableName === filterColumn && table.database === selectedDatabase);
-    if (selectedTable) {
-      handleSelectTable(selectedTable);
+    const FilterData = () => {
+        setFilterColumn(selectedTable ? selectedTable.tableName : "");
+        setShowData(true);
     }
-  };
 
-  return (
-    <div>
-      <FormControl fullWidth>
-        <InputLabel>Database</InputLabel>
-        <Select
-          value={selectedDatabase}
-          onChange={(e) => handleSelectDatabase(e.target.value as string)}
-        >
-          {Array.from(new Set(userData.map((data) => data.database))).map((database) => (
-            <MenuItem key={database} value={database}>
-              {database}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+    return (
 
-      <FormControl fullWidth>
-        <InputLabel>Table</InputLabel>
-        <Select
-          value={filterColumn}
-          onChange={(e) => handleFilterColumn(e.target.value as string)}
-          disabled={!selectedDatabase}
-        >
-          {userData
-            .filter((data) => data.database === selectedDatabase)
-            .map((table) => (
-              <MenuItem key={table.tableName} value={table.tableName}>
-                {table.tableName}
-              </MenuItem>
-            ))}
-        </Select>
-      </FormControl>
+        <div className='NavBar'>
 
-      <Button onClick={handlePreviewClick} disabled={!filterColumn}>Preview</Button>
+            <div className='logo'>
+                <p>middleware<br />system</p>
+            </div>
 
-      {!showData && (
-        <div className='notFound'>
-          <img src={NotFound} alt='NotFound' />
-          <p>Please select database and table</p>
+            <div className='chooses'>
+                <div className='dbSelect'>
+                    <select onChange={handleDatabaseChange}>
+                        <option value="NULL">Select a database</option>
+                        {data.map(db => (
+                            <option key={db.id} value={db.id}>{db.name}</option>
+                        ))}
+                    </select>
+                    <i className="bi bi-caret-down"></i>
+                </div>
+
+                <div className='tableSelect'>
+                    <select onChange={handleTableChange} disabled={disabledTable}>
+                        <option value="NULL">Select a table</option>
+                        {selectedDatabase?.tables.map(tb => (
+                            <option key={tb.tableId} value={tb.tableId}>{tb.tableName}</option>
+                        ))}
+                    </select>
+                    <i className="bi bi-caret-down"></i>
+                </div>
+
+                <div className='description'>
+                    <button onClick={() => { setShowDescription(prev => !prev) }} className={disabled || !selectedDatabase ? "disabled" : ""}>description</button>
+                    {!disabled && showDescription && selectedDatabase && (
+                        <div className='content'>
+                            <i className="bi bi-caret-up-fill"></i>
+                            <h3>{selectedTable?.description}</h3>
+                        </div>
+                    )}
+                </div>
+
+                <div className='preview'>
+                    <button onClick={()=>{FilterData()}} className={disabled || !selectedDatabase ? "disabledPreview" : ""}>preview</button>
+                </div>
+            </div>
         </div>
-      )}
-      {showData && (
-        <div className='viewData'>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {Object.keys(dataFilter[0] || {}).map((columnName) => (
-                    <TableCell key={columnName}>{columnName}</TableCell>
-                  ))}
-                  <TableCell>Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {tableData.map((row, rowIndex) => (
-                  <TableRow key={rowIndex}>
-                    {Object.keys(row).map((columnName) => (
-                      <TableCell key={columnName}>
-                        {editRowId === rowIndex ? (
-                          <TextField
-                            value={editFormData[columnName] || ''}
-                            onChange={(e) => handleInputChange(e, columnName)}
-                          />
-                        ) : (
-                          row[columnName]
-                        )}
-                      </TableCell>
-                    ))}
-                    <TableCell>
-                      {editRowId === rowIndex ? (
-                        <IconButton onClick={handleSaveClick}>
-                          <Save />
-                        </IconButton>
-                      ) : (
-                        <>
-                          <IconButton onClick={() => handleEditClick(row, rowIndex)}>
-                            <Edit />
-                          </IconButton>
-                          <IconButton onClick={() => handleDeleteClick(rowIndex)}>
-                            <Delete />
-                          </IconButton>
-                        </>
-                      )}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
-export default DataTable;
+export default FilterPanel;
+
+
+--------------------------------
