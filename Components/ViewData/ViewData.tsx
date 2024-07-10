fff55fae856036d53591357edf1a3ff4667ba7db
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-10 15:48:43 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,118 +1,3 @@
-import React, { useState } from 'react';
-import data from '../../Data/SchemaDB.json';
-import './FilterPanel.css';
-import { useTable } from '../CustomHook/CustomHook';
-
-interface Column {
-  columnId: number;
-  columnName: string;
-  dataType: string;
-}
-
-interface Table {
-  tableId: number;
-  tableName: string;
-  primaryKey: string;
-  description: string;
-  columns: Column[];
-}
-
-interface Database {
-  id: number;
-  name: string;
-  tables: Table[];
-}
-
-const FilterPanel = () => {
-  const [selectedDatabase, setSelectedDatabase] = useState<Database>();
-  const [disabled, setDisabled] = useState(true);
-  const [showDescription, setShowDescription] = useState(false);
-  const [disabledTable, setDisabledTable] = useState(true);
-  const { selectedTable, setSelectedTable, setFilterColumn, setShowData } = useTable();
-
-  const handleDatabaseChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const databaseId = parseInt(event.target.value);
-    const database = data.find(db => db.id === databaseId);
-    setSelectedDatabase(database);
-    if (event.target.value == "NULL") {
-      setShowDescription(false);
-      setShowData(false);
-      setDisabledTable(true);
-    } else {
-      setDisabledTable(false);
-    }
-  };
-
-  const handleTableChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const tableId = parseInt(event.target.value);
-    const table = selectedDatabase?.tables.find(tb => tb.tableId === tableId);
-    setSelectedTable(table);
-    if (event.target.value == "NULL") {
-      setDisabled(true);
-      setShowData(false);
-    } else {
-      setDisabled(false);
-    }
-  };
-
-  const FilterData = () => {
-    setFilterColumn(selectedTable ? selectedTable.tableName : "");
-    setShowData(true);
-  }
-
-  return (
-    <div className='NavBar'>
-      <div className='logo'>
-        <p>middleware<br />system</p>
-      </div>
-
-      <div className='chooses'>
-        <div className='dbSelect'>
-          <select onChange={handleDatabaseChange}>
-            <option value="NULL">Select a database</option>
-            {data.map(db => (
-              <option key={db.id} value={db.id}>{db.name}</option>
-            ))}
-          </select>
-          <i className="bi bi-caret-down"></i>
-        </div>
-
-        <div className='tableSelect'>
-          <select onChange={handleTableChange} disabled={disabledTable}>
-            <option value="NULL">Select a table</option>
-            {selectedDatabase?.tables.map(tb => (
-              <option key={tb.tableId} value={tb.tableId}>{tb.tableName}</option>
-            ))}
-          </select>
-          <i className="bi bi-caret-down"></i>
-        </div>
-
-        <div className='description'>
-          <button onClick={() => { setShowDescription(prev => !prev) }} className={disabled || !selectedDatabase ? "disabled" : ""}>description</button>
-          {!disabled && showDescription && selectedDatabase && (
-            <div className='content'>
-              <i className="bi bi-caret-up-fill"></i>
-              <h3>{selectedTable?.description}</h3>
-            </div>
-          )}
-        </div>
-
-        <div className='preview'>
-          <button onClick={FilterData} className={disabled || !selectedDatabase ? "disabledPreview" : ""}>preview</button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default FilterPanel;
-
-
-
----------
-
-
-
 import React, { useCallback, useState, useEffect } from 'react';
 import Button from 'devextreme-react/button';
 import DataGrid, {
@@ -125,7 +10,9 @@ import './DataTable.css';
 
 const DataTable = () => {
   const { selectedTable, showData, filterColumn } = useTable();
-  const [data, setData] = useState([]);
+
+  // Define the type for the data state
+  const [data, setData] = useState<Array<Record<string, any>>>([]);
 
   useEffect(() => {
     if (filterColumn) {
@@ -134,7 +21,7 @@ const DataTable = () => {
     }
   }, [filterColumn]);
 
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState<string[]>([]);
 
   const logEvent = useCallback((eventName: string) => {
     setEvents((previousEvents) => [eventName, ...previousEvents]);
@@ -201,39 +88,3 @@ const DataTable = () => {
 };
 
 export default DataTable;
-
-
-
-
-
-
--------------------------------------------
-
-  Argument of type '{ UserID: number; UserName: string; UserAge: number; Gender: string; }[] | { EmployeeID: number; EmployeeName: string; }[] | { CustomerID: number; CustomerName: string; }[] | { OrderID: number; OrderName: string; }[]' is not assignable to parameter of type 'SetStateAction<never[]>'.
-  Type '{ UserID: number; UserName: string; UserAge: number; Gender: string; }[]' is not assignable to type 'SetStateAction<never[]>'.
-    Type '{ UserID: number; UserName: string; UserAge: number; Gender: string; }[]' is not assignable to type 'never[]'.
-      Type '{ UserID: number; UserName: string; UserAge: number; Gender: string; }' is not assignable to type 'never'.ts(2345)
-const tableData: {
-    UserID: number;
-    UserName: string;
-    UserAge: number;
-    Gender: string;
-}[] | {
-    EmployeeID: number;
-    EmployeeName: string;
-}[] | {
-    CustomerID: number;
-    CustomerName: string;
-}[] | {
-    OrderID: number;
-    OrderName: string;
-}[]
-
-Argument of type '(previousEvents: never[]) => string[]' is not assignable to parameter of type 'SetStateAction<never[]>'.
-  Type '(previousEvents: never[]) => string[]' is not assignable to type '(prevState: never[]) => never[]'.
-    Type 'string[]' is not assignable to type 'never[]'.
-      Type 'string' is not assignable to type 'never'.ts(2345)
-
-
-
-  
