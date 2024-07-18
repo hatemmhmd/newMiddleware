7b5b0d4170419de6def0824dd2c5bd90391434a4
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-18 07:34:12 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,99 +1,55 @@
+import './Design.css';
+import DataGrid, { Column } from 'devextreme-react/data-grid';
+import data from './Data.json'
 
-[
-  {
-    "systemID": 1,
-    "pirid": 1,
-    "systemName": "Reflect",
-    "country": "Jordan",
-    "isActive": true
-  },
-  {
-    "systemID": 1,
-    "pirid": 1,
-    "systemName": "Reflect",
-    "country": "Jordan",
-    "isActive": true
-  },
-  {
-    "systemID": 1,
-    "pirid": 1,
-    "systemName": "Reflect",
-    "country": "Jordan",
-    "isActive": true
-  },
-  {
-    "systemID": 1,
-    "pirid": 1,
-    "systemName": "Reflect",
-    "country": "Jordan",
-    "isActive": false
-  },
-  {
-    "systemID": 1,
-    "pirid": 1,
-    "systemName": "Reflect",
-    "country": "Jordan",
-    "isActive": false
-  }
-]
+const home = () => {
+  const renderPlayIcon = () => (
+    <div className="icon-cell">
+      <i className="dx-icon-play"></i>
+    </div>
+  );
 
---------
-using Microsoft.AspNetCore.Mvc;
- 
-namespace PIR.API.Controllers
-{
- 
-    public class AdminstartionDTO
-    {
-        public int SystemID { get; set; }
-        public int PIRID { get; set; }
-        public string? SystemName { get; set; }
-        public string? Country { get; set; }
-        public bool IsActive { get; set; }
-    }
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AdminstrationController : ControllerBase
-    {
-        [HttpGet]
-        public ActionResult<List<AdminstartionDTO>> GetSystemInfo()
-        {
-            List<AdminstartionDTO> systemInfoList = new List<AdminstartionDTO>
-            {
-                new AdminstartionDTO {
-                    SystemID = 1,
-                    PIRID = 1,
-                    SystemName = "Reflect",
-                    Country = "Jordan",
-                    IsActive = true 
-                },
-                new AdminstartionDTO { SystemID = 1,
-                    PIRID = 1,
-                    SystemName = "Reflect",
-                    Country = "Jordan",
-                    IsActive = true
-                },
-                new AdminstartionDTO {SystemID = 1,
-                    PIRID = 1,
-                    SystemName = "Reflect",
-                    Country = "Jordan",
-                    IsActive = true 
-                },
-                new AdminstartionDTO {SystemID = 1,
-                    PIRID = 1,
-                    SystemName = "Reflect",
-                    Country = "Jordan",
-                    IsActive = false 
-                },
-                new AdminstartionDTO { SystemID = 1,
-                    PIRID = 1,
-                    SystemName = "Reflect",
-                    Country = "Jordan",
-                    IsActive = false
-                }
-            };
- 
-            return Ok(systemInfoList);
-        }
-    }
+  return (
+    <div className="data-grid-container">
+      <DataGrid
+        dataSource={data}
+        showBorders={true}
+        rowAlternationEnabled={true}
+        height="100%"
+      >
+        <Column dataField="systemID" caption="System ID" alignment="center" />
+        <Column dataField="pirid" caption="PIR ID" alignment="center" />
+        <Column dataField="systemName" caption="System Name" alignment="center" />
+        <Column dataField="country" caption="Country" alignment="center" />
+        <Column dataField="isActive" caption="Is Active" dataType="boolean" alignment="center" />
+        <Column caption="Action" alignment="center" cellRender={renderPlayIcon} />
+      </DataGrid>
+    </div>
+  );
+};
+
+export default home;
+
+
+------------------------------- 
+
+  .data-grid-container .dx-datagrid-headers {
+    background-color: #111 !important;
+    /* Change header color */
 }
+
+.data-grid-container .dx-header-row .dx-datagrid-text-content {
+    text-align: center;
+    /* Center header text */
+}
+
+.data-grid-container .dx-data-row .dx-datagrid-text-content {
+    text-align: center;
+    padding: 24px 0 !important;
+}
+
+.data-grid-container {
+    height: 100vh !important;
+}
+
+
