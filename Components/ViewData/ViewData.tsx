00d157048dc196063b2d439428280b19f46531b5
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-18 09:00:28 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,3 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import DataGrid, { Column } from 'devextreme-react/data-grid';
+import 'devextreme/dist/css/dx.light.css'; // Import DevExtreme styles
+import './DataGridComponent.css'; // Import custom styles
+import axios from 'axios';
+
+const DataGridComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://your-api-endpoint.com/data');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const renderPlayIcon = () => (
+    <div className="icon-cell">
+      <i className="dx-icon dx-icon-video"></i> {/* Use an available DevExtreme icon */}
+    </div>
+  );
+
+  return (
+    <div className="data-grid-container">
+      <DataGrid
+        dataSource={data}
+        showBorders={true}
+        rowAlternationEnabled={true}
+        height="100%"
+      >
+        <Column dataField="systemID" caption="System ID" alignment="center" />
+        <Column dataField="pirid" caption="PIR ID" alignment="center" />
+        <Column dataField="systemName" caption="System Name" alignment="center" />
+        <Column dataField="country" caption="Country" alignment="center" />
+        <Column dataField="isActive" caption="Is Active" dataType="boolean" alignment="center" />
+        <Column caption="Action" alignment="center" cellRender={renderPlayIcon} />
+      </DataGrid>
+    </div>
+  );
+};
+
+export default DataGridComponent;
+
+
+
+-----------
 import DataGrid, { Column } from 'devextreme-react/data-grid';
 import 'devextreme/dist/css/dx.light.css'; // Import DevExtreme styles
 import './Design.css';
