a1f36388b573c98f26c6aaa801b1df94aab095da
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-28 12:47:25 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,14 +1,18 @@
-  const sendDataToAPI = async (systemID: number, startDate: string | null, endDate: string | null, editingMode: string) => {
-    try {
-      //`${process.env.REACT_APP_BACKEND_API}api/Adminstration/insertpir${editingMode}`
-      await fetch("https://localhost:44382/api/Adminstration/insertpir", {
-        method: 'POST',
+import axios from 'axios';
+
+const sendDataToAPI = async (systemID: number, startDate: string | null, endDate: string | null, editingMode: string) => {
+  try {
+    await axios.post(
+      `https://localhost:44382/api/Adminstration/insertpir`,
+      { systemID, startDate, endDate },
+      {
         headers: {
           'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ systemID, startDate, endDate }),
-      });
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  };
+        }
+      }
+    );
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
+ 
