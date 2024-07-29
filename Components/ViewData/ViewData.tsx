4d2e6e83bf55428fff72456d6abfe262d484372e
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-29 16:01:51 +0300

    Update ViewData.taxes

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,84 +1,50 @@
-import TabPanel from 'devextreme-react/tab-panel';
+import React from 'react';
 import DataGrid, { Column, FilterRow, Pager, Paging, Button as GridButton, SearchPanel, Grouping, GroupPanel } from 'devextreme-react/data-grid';
-import { dataSource } from './data';
 import { useNavigate } from 'react-router-dom';
 import './PIRSchedule.css'
 
-
 const App = () => {
     const navigate = useNavigate();
-    const showData = (pirId: number, systemname: string, stutes: string) => {
-        navigate(`/dataview/${pirId}/${systemname}/${stutes}`);
+    const showData = (pirId, systemname, status) => {
+        navigate(`/dataview/${pirId}/${systemname}/${status}`);
     };
 
-    const schedulesGrid = (data: any) => {
-        return <DataGrid
-            dataSource={data.data.tasks}
-            hoverStateEnabled
-            height={"70vh"}
-        >
-            <FilterRow visible={true} applyFilter='auto' />
-
-            <Paging defaultPageSize={50} />
-
-            <Pager
-                visible={true}
-                allowedPageSizes={[10, 20, 50]}
-                showPageSizeSelector={true}
-                showInfo={true}
-                showNavigationButtons={true} />
-
-            <SearchPanel visible={true} placeholder='Search System' width={300} />
-
-            <Column
-                dataField="PIRID"
-                caption="PIR ID"
-                width={"7%"}
-                alignment='left'
-            />
-
-            <Column
-                dataField="SystemName"
-                caption="SYSTEM"
-                width={"20%"}
-                groupIndex={0}
-                allowSearch={true}
-            />
-
-            <Column
-                dataField="Country"
-                caption="COUNTRY"
-                width={"20%"}
-                allowSearch={true}
-            />
-
-            <Column
-                width={"25%"}
-                dataField="StartDate"
-                caption="START DATE"
-                dataType="datetime"
-                format={"dd/MM/yyyy, HH:mm"}
-            />
-
-            <Column
-                width={"25%"}
-                dataField="EndDate"
-                caption="END DATE"
-                dataType="datetime"
-                format={"dd/MM/yyyy, HH:mm"}
-            />
-
-            <Column type='buttons' width={"15%"} caption="ACTIONS" >
-                <GridButton icon="chart" onClick={(e) => showData(e.row?.data.PIRID, e.row?.data.SystemName, "Technical")} text='Technical' />
-                <GridButton icon="toolbox" onClick={(e) => showData(e.row?.data.PIRID, e.row?.data.SystemName, "Business")} text='Business' />
-            </Column>
-
-            <Grouping contextMenuEnabled={false} expandMode='buttonClick' />
-            <GroupPanel visible={true} emptyPanelText='' />
-
-
-        </DataGrid >
-    }
+    const schedulesGrid = (data) => {
+        return (
+            <DataGrid
+                dataSource={data.data.tasks}
+                hoverStateEnabled
+                height={"70vh"}
+                onRowPrepared={(e) => {
+                    if (e.rowType === 'data' && e.data.SystemName !== data.data.tasks[0].SystemName) {
+                        e.rowElement.style.display = 'none';
+                    }
+                }}
+            >
+                <FilterRow visible={true} applyFilter='auto' />
+                <Paging defaultPageSize={50} />
+                <Pager
+                    visible={true}
+                    allowedPageSizes={[10, 20, 50]}
+                    showPageSizeSelector={true}
+                    showInfo={true}
+                    showNavigationButtons={true}
+                />
+                <SearchPanel visible={true} placeholder='Search System' width={300} />
+                <Column dataField="PIRID" caption="PIR ID" width={"7%"} alignment='left' />
+                <Column dataField="SystemName" caption="SYSTEM" width={"20%"} groupIndex={0} allowSearch={true} />
+                <Column dataField="Country" caption="COUNTRY" width={"20%"} allowSearch={true} />
+                <Column dataField="StartDate" caption="START DATE" width={"25%"} dataType="datetime" format={"dd/MM/yyyy, HH:mm"} />
+                <Column dataField="EndDate" caption="END DATE" width={"25%"} dataType="datetime" format={"dd/MM/yyyy, HH:mm"} />
+                <Column type='buttons' width={"15%"} caption="ACTIONS">
+                    <GridButton icon="chart" onClick={(e) => showData(e.row.data.PIRID, e.row.data.SystemName, "Technical")} text='Technical' />
+                    <GridButton icon="toolbox" onClick={(e) => showData(e.row.data.PIRID, e.row.data.SystemName, "Business")} text='Business' />
+                </Column>
+                <Grouping contextMenuEnabled={false} expandMode='buttonClick' />
+                <GroupPanel visible={true} emptyPanelText='' />
+            </DataGrid>
+        );
+    };
 
     return (
         <div className="tabpanel-demo">
@@ -93,7 +59,6 @@ const App = () => {
                     stylingMode="primary"
                     tabsPosition='top'
                     itemComponent={schedulesGrid}
-
                 />
             </div>
         </div>
