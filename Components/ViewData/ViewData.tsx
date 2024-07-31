908c750ce21a68a1ac8fdf6a846018be4f65d080
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-07-31 15:16:50 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,68 +1,539 @@
-import React from 'react';
-import DataGrid, { Column, FilterRow, Pager, Paging, Button as GridButton, SearchPanel, Grouping, GroupPanel } from 'devextreme-react/data-grid';
-import { useNavigate } from 'react-router-dom';
-import './PIRSchedule.css'
+const recentPIRs = [
+    {
+        "PIRID": 1,
+        "SystemName": "Arabia",
+        "Country": ["Jordan, Palastine"],
+        "StartDate": '17/7/2024, 2:40 PM',
+        "EndDate": '21/2/2024, 1:45 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 2,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '20/7/2024, 11:40 PM',
+        "EndDate": '22/7/2024, 3:45 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 3,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '15/7/2024, 12:40 PM',
+        "EndDate": '18/7/2024, 1:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 4,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '21/7/2024, 1:10 PM',
+        "EndDate": '22/7/2024, 6:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 5,
+        "SystemName": "Reflect",
+        "Country": ["Jordan , Lebanon"],
+        "StartDate": '16/7/2024, 1:40 PM',
+        "EndDate": '18/7/2024, 3:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 6,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '18/6/2024, 9:00 PM',
+        "EndDate": '20/6/2024, 11:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 7,
+        "SystemName": "Reflect",
+        "Country": ["Jordan"],
+        "StartDate": '18/6/2024, 9:00 PM',
+        "EndDate": '20/6/2024, 11:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 8,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '18/6/2024, 9:00 PM',
+        "EndDate": '20/6/2024, 11:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 9,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '21/6/2024, 9:20 PM',
+        "EndDate": '22/6/2024, 2:20 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 10,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '23/6/2024, 3:20 PM',
+        "EndDate": '26/6/2024, 6:20 PM',
+        "isRunning": true
+    },
 
-const App = () => {
-    const navigate = useNavigate();
-    const showData = (pirId, systemname, status) => {
-        navigate(`/dataview/${pirId}/${systemname}/${status}`);
-    };
+    {
+        "PIRID": 11,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '1/6/2024, 09:00 AM',
+        "EndDate": '3/6/2024, 12:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 12,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '4/6/2024, 10:00 AM',
+        "EndDate": '4/6/2024, 1:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 13,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '5/6/2024, 11:00 AM',
+        "EndDate": '7/6/2024, 1:00 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 14,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '8/6/2024, 12:00 AM',
+        "EndDate": '10/6/2024, 1:00 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 15,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '11/6/2024, 02:00 AM',
+        "EndDate": '12/6/2024, 4:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 16,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '13/6/2024, 10:00 PM',
+        "EndDate": '15/6/2024, 1:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 17,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 1:00 PM',
+        "EndDate": '4/6/2024, 2:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 18,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 12:00 AM',
+        "EndDate": '4/6/2024, 2:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 19,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 2:00 PM',
+        "EndDate": '4/6/2024, 4:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 20,
+        "SystemName": "Reflect",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 5:00 PM',
+        "EndDate": '4/6/2024, 6:00 PM',
+        "isRunning": true
+    },
 
-    const schedulesGrid = (data) => {
-        return (
-            <DataGrid
-                dataSource={data.data.tasks}
-                hoverStateEnabled
-                height={"70vh"}
-                onRowPrepared={(e) => {
-                    if (e.rowType === 'data' && e.data.SystemName !== data.data.tasks[0].SystemName) {
-                        e.rowElement.style.display = 'none';
-                    }
-                }}
-            >
-                <FilterRow visible={true} applyFilter='auto' />
-                <Paging defaultPageSize={50} />
-                <Pager
-                    visible={true}
-                    allowedPageSizes={[10, 20, 50]}
-                    showPageSizeSelector={true}
-                    showInfo={true}
-                    showNavigationButtons={true}
-                />
-                <SearchPanel visible={true} placeholder='Search System' width={300} />
-                <Column dataField="PIRID" caption="PIR ID" width={"7%"} alignment='left' />
-                <Column dataField="SystemName" caption="SYSTEM" width={"20%"} groupIndex={0} allowSearch={true} />
-                <Column dataField="Country" caption="COUNTRY" width={"20%"} allowSearch={true} />
-                <Column dataField="StartDate" caption="START DATE" width={"25%"} dataType="datetime" format={"dd/MM/yyyy, HH:mm"} />
-                <Column dataField="EndDate" caption="END DATE" width={"25%"} dataType="datetime" format={"dd/MM/yyyy, HH:mm"} />
-                <Column type='buttons' width={"15%"} caption="ACTIONS">
-                    <GridButton icon="chart" onClick={(e) => showData(e.row.data.PIRID, e.row.data.SystemName, "Technical")} text='Technical' />
-                    <GridButton icon="toolbox" onClick={(e) => showData(e.row.data.PIRID, e.row.data.SystemName, "Business")} text='Business' />
-                </Column>
-                <Grouping contextMenuEnabled={false} expandMode='buttonClick' />
-                <GroupPanel visible={true} emptyPanelText='' />
-            </DataGrid>
-        );
-    };
+    {
+        "PIRID": 21,
+        "SystemName": "COB",
+        "Country": ["Jordan"],
+        "StartDate": '1/6/2024, 12:00 AM',
+        "EndDate": '2/6/2024, 04:10 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 22,
+        "SystemName": "COB",
+        "Country": ["Jordan"],
+        "StartDate": '3 / 6 / 2024, 02:00 AM',
+        "EndDate": '3/6/2024, 04:10 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 23,
+        "SystemName": "Helios",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '4/6/2024, 01:00 AM',
+        "EndDate": '6/6/2024, 03:10 AM',
+        "isRunning": true
+    },
+];
 
-    return (
-        <div className="tabpanel-demo">
-            <div className="widget-container">
-                <TabPanel
-                    width="100%"
-                    height="100%"
-                    animationEnabled={true}
-                    swipeEnabled={true}
-                    dataSource={dataSource}
-                    iconPosition="top"
-                    stylingMode="primary"
-                    tabsPosition='top'
-                    itemComponent={schedulesGrid}
-                />
-            </div>
-        </div>
-    );
-};
 
-export default App;
+const PIRHistory = [
+    {
+        "PIRID": 1,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '1/6/2024, 9:20 AM',
+        "EndDate": '2/6/2024, 2:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 2,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '3/6/2024, 10:00 AM',
+        "EndDate": '5/6/2024, 10:00 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 3,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '6/6/2024, 9:20 AM',
+        "EndDate": '8/6/2024, 9:20 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 4,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '9/6/2024, 9:20 AM',
+        "EndDate": '9/6/2024, 2:20 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 5,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '10/6/2024, 9:20 AM',
+        "EndDate": '12/6/2024, 12:20 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 6,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '13/6/2024, 9:20 PM',
+        "EndDate": '15/6/2024, 2:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 7,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '16/6/2024, 10:00 PM',
+        "EndDate": '17/6/2024, 12:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 8,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '18/6/2024, 9:00 PM',
+        "EndDate": '20/6/2024, 11:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 9,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '21/6/2024, 9:20 PM',
+        "EndDate": '22/6/2024, 2:20 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 10,
+        "SystemName": "Arabia",
+        "Country": ["Jordan , Palastine"],
+        "StartDate": '23/6/2024, 3:20 PM',
+        "EndDate": '26/6/2024, 6:20 PM',
+        "isRunning": true
+    },
+
+    {
+        "PIRID": 11,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '1/6/2024, 09:00 AM',
+        "EndDate": '3/6/2024, 12:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 12,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '4/6/2024, 10:00 AM',
+        "EndDate": '4/6/2024, 1:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 13,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '5/6/2024, 11:00 AM',
+        "EndDate": '7/6/2024, 1:00 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 14,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '8/6/2024, 12:00 AM',
+        "EndDate": '10/6/2024, 1:00 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 15,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '11/6/2024, 02:00 AM',
+        "EndDate": '12/6/2024, 4:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 16,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '13/6/2024, 10:00 PM',
+        "EndDate": '15/6/2024, 1:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 17,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 1:00 PM',
+        "EndDate": '4/6/2024, 2:30 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 18,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 12:00 AM',
+        "EndDate": '4/6/2024, 2:30 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 19,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 2:00 PM',
+        "EndDate": '4/6/2024, 4:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 20,
+        "SystemName": "FundBot",
+        "Country": ["Jordan"],
+        "StartDate": '2/6/2024, 5:00 PM',
+        "EndDate": '4/6/2024, 6:00 PM',
+        "isRunning": true
+    },
+
+    {
+        "PIRID": 21,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '1/6/2024, 12:00 AM',
+        "EndDate": '2/6/2024, 04:10 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 22,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '3 / 6 / 2024, 02:00 AM',
+        "EndDate": '3/6/2024, 04:10 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 23,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '4/6/2024, 01:00 AM',
+        "EndDate": '6/6/2024, 03:10 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 24,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '7/6/2024, 12:00 AM',
+        "EndDate": '9/6/2024, 02:00 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 25,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '10/6/2024, 12:00 AM',
+        "EndDate": '10/6/2024, 06:00 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 26,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '11/6/2024, 12:10 PM',
+        "EndDate": '13/6/2024, 3:10 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 27,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '12/6/2024, 01:00 PM',
+        "EndDate": '14/6/2024, 04:00 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 28,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '12/6/2024, 02:10 PM',
+        "EndDate": '14/6/2024, 04:10 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 29,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '12/6/2024, 05:00 PM',
+        "EndDate": '14/6/2024, 06:10 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 30,
+        "SystemName": "COB",
+        "Country": ["Jordan , Morocco"],
+        "StartDate": '15/6/2024, 12:00 PM',
+        "EndDate": '15/6/2024, 4:10 PM',
+        "isRunning": true
+    },
+
+
+    {
+        "PIRID": 31,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '1/6/2024, 02:20 AM',
+        "EndDate": '3/6/2024, 03:50 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 32,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '4/6/2024, 12:20 AM',
+        "EndDate": '4/6/2024, 03:50 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 34,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '5/6/2024, 04:20 AM',
+        "EndDate": '6/6/2024, 09:50 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 35,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '7/6/2024, 6:20 AM',
+        "EndDate": '9/6/2024, 8:50 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 36,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '10/6/2024, 2:20 AM',
+        "EndDate": '12/6/2024, 5:50 AM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 37,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '13/6/2024, 3:50 PM',
+        "EndDate": '15/6/2024, 6:50 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 38,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '16/6/2024, 2:20 PM',
+        "EndDate": '18/6/2024, 3:50 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 39,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '19/6/2024, 6:20 PM',
+        "EndDate": '23/6/2024, 9:50 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 40,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '26/6/2024, 9:00 PM',
+        "EndDate": '28/6/2024, 12:50 PM',
+        "isRunning": true
+    },
+    {
+        "PIRID": 41,
+        "SystemName": "Helios",
+        "Country": ["Jordan"],
+        "StartDate": '29/6/2024, 7:20 PM',
+        "EndDate": '30/6/2024, 12:50 PM',
+        "isRunning": true
+    },
+
+
+    {
+        "PIRID": 42,
+        "SystemName": "Reflect",
+        "Country": ["Jordan , Lebanon"],
+        "StartDate": '1/6/2024, 8:00 AM',
+        "EndDate": '3/6/2024, 12:10 AM',
+        "isRunning": true
+    }]
+
+
+export const dataSource = [
+    {
+        icon: 'description',
+        title: 'Recent PIR',
+        tasks: recentPIRs,
+    },
+    {
+        icon: 'taskhelpneeded',
+        title: 'PIR history',
+        tasks: PIRHistory
+    },
+];
