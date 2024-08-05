2bdf9e6eb8b27d5ad6021f3685e7138b1947e372
hatemmhmd <hatem.mohmmad120@gmail.com>
2024-08-05 12:09:06 +0300

    Update ViewData.tsx

--- Components/ViewData/ViewData.tsx
+++ Components/ViewData/ViewData.tsx
@@ -1,139 +1,210 @@
+import React, { useEffect, useRef, useState } from 'react';
+import DataGrid, { Column, Editing, FilterRow, Pager, Paging, Button as GridButton, Scrolling } from 'devextreme-react/data-grid';
+import DateBox from 'devextreme-react/date-box';
+import { Button } from 'devextreme-react/button';
+import 'devextreme/dist/css/dx.light.css';
+import './Adminstration.css';
+import { TagBox } from 'devextreme-react';
+import axios from 'axios';
+import { createCustomStore } from './custom-store';
 import CustomStore from 'devextreme/data/custom_store';
-import axios, { AxiosError } from 'axios';
-import notify from 'devextreme/ui/notify';
-import _ from 'lodash';
 
-export function createCustomStore() {
-
-
-    let fullDataCache: any[] = [];
-
-    const sucessHandler = (mode: "update" | "delete") => {
-        let msg = `${mode.toUpperCase()} request submitted successfully! It is on the way for Checker review`;
-        notify(msg, "success", 3000);
-    }
-
-    const exceptionHandler = (error: any) => {
-        if (error instanceof AxiosError) {
-            if (axios.isAxiosError(error) && error.response?.status === 401) {
-            }
-        } else if (typeof error === "string") {
-            let msg = `Error: ${error}`;
-            notify(msg, "error", 3000);
-        }
-    }
-
-    function isNotEmpty(value: string | undefined | null) {
-        return value !== undefined && value !== null && value !== '';
-    }
-
-    const myStore = new CustomStore({
-        load: (loadOptions: any) => {
-            const paramNames = [
-                'skip', 'take', 'requireTotalCount', 'requireGroupCount',
-                'sort', 'filter', 'totalSummary', 'group', 'groupSummary',
-            ];
-
-            const queryString = paramNames
-                .filter((paramName) => isNotEmpty(loadOptions[paramName]))
-                .map((paramName) => `${paramName}=${JSON.stringify(loadOptions[paramName])}`)
-                .join('&');
-
-            return new Promise((resolve) => {
-                try {
-                    axios.get(`https://localhost:44382/api/Adminstration?`)
-                        .then(async response => {
-                            fullDataCache = response.data.data;
-                            const totalCount = response.data.totalCount;
-                            resolve({
-                                data: fullDataCache,
-                                totalCount: totalCount || 1
-                            });
-                        }).catch(error => {
-                            exceptionHandler(error);
-                            resolve({
-                                data: [],
-                                totalCount: 0
-                            });
-                        });
-                } catch (error) {
-                    console.error('Error in load function:', error);
-                    resolve({
-                        data: [],
-                        totalCount: 0
-                    });
-                }
-            });
-        },
-
-
-        update: (key: any, values: any) => {
-            const api = key.startDate == null && key.endDate == null
-                ? "https://localhost:44382/api/Adminstration/insertpir"
-                : "https://localhost:44382/api/Adminstration/updatepir"
-            let result: any = _.cloneDeep({ ...key, ...values });
-
-            console.log(key)
-
-            const today = new Date().setHours(0, 0, 0, 0);
-            if (result.startDate == null || result.endDate == null) {
-                notify('Start And End Date Is Required', 'error', 2000)
-                return Promise.resolve();
-            }
-            if (result.startDate && new Date(result.startDate).getTime() < today) {
-                notify('Start Date Cannot Be In The Past', 'error', 2000);
-                return Promise.resolve();
-            }
-            if (result.endDate && new Date(result.endDate).getTime() < new Date(result.startDate || result.startDate).getTime()) {
-                notify('End date must be greater than or equal to the start date', 'warning', 2000);
-                return Promise.resolve();
-            }
-
-
-
-
-            if (result.pirid == null) {
-                return axios.post(
-                    api,
-                    {
-                        systemID: result.systemID,
-                        StartDate: result.startDate,
-                        EndDate: result.endDate
-                    },
-                    {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    }
-                )
-                    .then(() => {
-                        sucessHandler("update");
-                    }).catch((error) => {
-                        exceptionHandler(error);
-                    })
-            }
-            else {
-                return axios.put(
-                    api,
-                    {
-                        systemID: result.systemID,
-                        PIRId: result.pirid,
-                        StartDate: result.startDate,
-                        EndDate: result.endDate
-                    },
-                    {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    }
-                )
-                    .then(() => {
-                        sucessHandler("update");
-                    }).catch((error) => {
-                        exceptionHandler(error);
-                    })
-            }
-        },
-    });
-    return myStore;
+interface System {
+  systemID: number;
+  pirid: number | null;
+  systemName: string;
+  country: string[];
+  startDate: Date | null;
+  endDate: Date | null;
+  isRunning: boolean;
 }
+
+type SystemField = keyof System;
+
+const GridTable: React.FC = () => {
+  const [editingRowKey, setEditingRowKey] = useState<number | null>(null);
+  const customstore = createCustomStore();
+
+
+  useEffect(() => {
+    setInterval(() => {
+      window.location.reload();
+    }, 180_000)
+  }, [])
+
+
+
+
+  const dateCellRender = (cellData: any, dateField: SystemField) => (
+    <DateBox
+      placeholder='Please Enter Date'
+      type='datetime'
+      value={cellData.data[dateField]}
+      displayFormat='dd/MM/yyyy  hh:mm aa'
+      readOnly
+    />
+  );
+
+  const systemNameCellRender = (cellData: any) => (
+    <div className="flex-container-system-name">
+      <div>
+        {cellData.data.systemName}
+      </div>
+      <div>
+        {cellData.data.isRunning && (
+          <div className='pirId-spinner'>
+            <div className="running-text"> <Button icon="datapie" className='spinner' /> </div>
+            <div className='pirId'>{cellData.data.pirid}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+
+  const countryCellRender = (cellData: any) => (
+    <TagBox readOnly value={cellData.data.country?.split(',')}
+      dataSource={[]}
+      showClearButton={false}
+      stylingMode="underlined"
+    />
+  );
+
+  const OnStop = async (pirId: number) => {
+    try {
+      await axios.put(`https://localhost:44382/api/Adminstration/Stoppir`,
+        pirId,
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+    } catch (error) {
+      console.log('Error:', error);
+    }
+    window.location.reload();
+  };
+
+
+
+  const onEditCanceling = (e: any) => {
+    setCheck(true);
+  };
+
+
+  const [startDate, setStartDate] = useState<boolean>(false);
+  const [endDate, setEndDate] = useState<boolean>(false);
+  const [check, setCheck] = useState<boolean>(false);
+
+
+
+  const [currentEditedPirID, setCurrentEditedPirID] = useState<number>();
+
+
+
+  const onEditingStart = (e: any) => {
+
+    console.log(currentEditedPirID)
+
+
+    if (e.data.isRunning) {
+      setStartDate(false);
+      setEndDate(true);
+    }
+
+    else {
+      setStartDate(true);
+      setEndDate(true);
+    }
+  }
+
+  const onSave = () => {
+    setCheck(true);
+  }
+
+
+  return (
+    <DataGrid
+      dataSource={customstore}
+      onEditCanceling={onEditCanceling}
+      hoverStateEnabled={true}
+      onEditingStart={onEditingStart}
+      onSaved={onSave}
+    >
+      <Scrolling mode="standard" />
+
+
+
+      <Editing
+        mode="row"
+        allowUpdating={true}
+        useIcons={true}
+        startEditAction="click"
+      />
+
+      <FilterRow visible={true} />
+      <Paging defaultPageSize={50} />
+      <Pager
+        visible={true}
+        allowedPageSizes={[10, 20, 50]}
+        showPageSizeSelector={true}
+        showInfo={true}
+        showNavigationButtons={true}
+      />
+
+      <Column
+        dataField="systemName"
+        caption="SYSTEM"
+        cellRender={systemNameCellRender}
+        width={"20%"}
+        allowEditing={false}
+      />
+
+      <Column
+        dataField="country"
+        caption="COUNTRY"
+        allowEditing={false}
+        width={"25%"}
+        cellRender={countryCellRender}
+      />
+
+      <Column
+        width={"25%"}
+        dataField="startDate"
+        caption="START DATE"
+        dataType="datetime"
+        cellRender={(cellData) => dateCellRender(cellData, 'startDate')}
+        editorOptions={{
+          displayFormat: "dd/MM/yyyy  hh:mm aa",
+          showClearButton: true,
+          stylingMode: "underlined"
+        }}
+        allowEditing={startDate}
+      />
+
+      <Column
+        width={"25%"}
+        dataField="endDate"
+        caption="END DATE"
+        dataType="datetime"
+        cellRender={(cellData) => dateCellRender(cellData, 'endDate')}
+        editorOptions={{
+          displayFormat: "dd/MM/yyyy  hh:mm aa",
+          showClearButton: true,
+          stylingMode: "underlined"
+        }}
+        allowEditing={endDate}
+      />
+
+      <Column type="buttons" caption="ACTIONS" width={"25%"}>
+
+        <GridButton name="edit" icon="event" text='Edit' />
+        <GridButton name="edit" icon="square" cssClass={"action-stop"} visible={(e) => e.row.data.isRunning} onClick={(e) => { OnStop(e.row?.data.pirid) }} text='Stop' />
+        <GridButton name="edit" icon="chart" text='Dashboard' visible={true} />
+        <GridButton name="edit" icon="toolbox" onClick={() => null} visible={(e) => e.row.data.isRunning && check} text='Details' />
+        <GridButton name="edit" icon="download" onClick={() => null} visible={(e) => e.row.data.isRunning && check} text='Download' />
+      </Column>
+    </DataGrid>
+  );
+};
+
+export default GridTable;
